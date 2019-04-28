fc58c06e050231b71533da3b6f92f648eed4af0e
Yash Manian <yash.manian@iamrobotics.com>
2019-04-28 09:24:59 -0400

    Cleaned up my mess of ignorance

--- cmake-build-debug/CMakeFiles/vo.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/vo.dir/DependInfo.cmake
@@ -4,17 +4,16 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/yashmanian/CPP/MonocularVO/src/Tests/epiLines.cpp" "/home/yashmanian/CPP/MonocularVO/cmake-build-debug/CMakeFiles/vo.dir/src/Tests/epiLines.cpp.o"
-  "/home/yashmanian/CPP/MonocularVO/src/srcFiles/FeatTrack.cpp" "/home/yashmanian/CPP/MonocularVO/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/FeatTrack.cpp.o"
-  "/home/yashmanian/CPP/MonocularVO/src/srcFiles/PointOps.cpp" "/home/yashmanian/CPP/MonocularVO/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/PointOps.cpp.o"
-  "/home/yashmanian/CPP/MonocularVO/src/srcFiles/epipolar.cpp" "/home/yashmanian/CPP/MonocularVO/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/epipolar.cpp.o"
+  "/home/yashmanian/git/Monocular-Visual-Odometry/src/Tests/epiLines.cpp" "/home/yashmanian/git/Monocular-Visual-Odometry/cmake-build-debug/CMakeFiles/vo.dir/src/Tests/epiLines.cpp.o"
+  "/home/yashmanian/git/Monocular-Visual-Odometry/src/srcFiles/FeatTrack.cpp" "/home/yashmanian/git/Monocular-Visual-Odometry/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/FeatTrack.cpp.o"
+  "/home/yashmanian/git/Monocular-Visual-Odometry/src/srcFiles/PointOps.cpp" "/home/yashmanian/git/Monocular-Visual-Odometry/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/PointOps.cpp.o"
+  "/home/yashmanian/git/Monocular-Visual-Odometry/src/srcFiles/epipolar.cpp" "/home/yashmanian/git/Monocular-Visual-Odometry/cmake-build-debug/CMakeFiles/vo.dir/src/srcFiles/epipolar.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../include"
-  "/usr/local/include"
   "/usr/local/include/opencv"
   "/usr/local/include/eigen3"
   )
